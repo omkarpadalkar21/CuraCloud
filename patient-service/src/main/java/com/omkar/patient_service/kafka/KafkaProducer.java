65f39f2358431f16045ff67e7f424c86e6132996
omkar <omkarpadalkar21@gmail.com>
2026-03-02 23:46:21 +0530

    feat: Add kafka to produce patient creation event.

---
+++ patient-service/src/main/java/com/omkar/patient_service/kafka/KafkaProducer.java
@@ -0,0 +1,33 @@
+package com.omkar.patient_service.kafka;
+
+import com.omkar.patient_service.model.Patient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Slf4j
+@Service
+public class KafkaProducer {
+    // Used to define message types and send those messages
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
+
+        try {
+            kafkaTemplate.send("patient", event.toByteArray());
+        } catch (Exception e) {
+            log.error("Error sending PatientCreated event: {}", event);
+        }
+    }
+}
